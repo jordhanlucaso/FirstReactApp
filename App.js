285defcaf57caf2fa1f08f06032571dde0424d64
Jordhan L. Oliveira <jlos94@hotmail.com>
2021-01-28 20:32:25 +0100

    Starting app tp check setup

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello Jordhan, this is your first React Native App!</Text>
       <StatusBar style="auto" />
     </View>
   );
